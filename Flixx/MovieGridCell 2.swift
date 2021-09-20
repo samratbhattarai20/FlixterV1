267e83749de698c55d266a03a7f0738e2274bd7b
samratbhattarai20 <archanbhattarai@gmail.com>
2021-09-20 17:17:36 -0500

    Retrying

---
+++ Flixx/MovieGridCell 2.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixx
+//
+//  Created by Archan Bhattarai on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
